8c3914a077fe0b948fe8e5c9d6603f4160c9ae45
brandon <ruipengyu.li@gmail.com>
2024-01-17 18:35:18 +0000

    minor change

--- myday-frontend/src/pages/home/Home.jsx
+++ myday-frontend/src/pages/home/Home.jsx
@@ -1,6 +1,9 @@
 import "./Home.css";
 function Home() {
-  return <div className="home">Welcome to React Admin</div>;
+  return <>
+    <div className="home"> Welcome to MyDay </div>
+    <div className="subTitle">A activity tracker to record your meals and exercises every day</div>;
+  </>
 }
 
 export default Home;
